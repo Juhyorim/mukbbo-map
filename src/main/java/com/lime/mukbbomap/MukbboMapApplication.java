2eadaf2d1cf0f3567fd8ac5a9c7919251067b2f5
Juhyorim <joohr1234@gmail.com>
2026-01-06 23:40:27 +0900

    initial commit!

---
+++ src/main/java/com/lime/mukbbomap/MukbboMapApplication.java
@@ -0,0 +1,13 @@
+package com.lime.mukbbomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukbboMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MukbboMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lime/mukbbomap/MukbboMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lime.mukbbomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukbboMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
